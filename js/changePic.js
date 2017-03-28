a4813208dc980f1c81626d591b496b9a661d63b8
RagingGrim <armandconnor.duplooy@gmail.com>
2017-03-28 11:07:05 +0200

    Updates.

--- js/changePic.js
+++ js/changePic.js
@@ -1,1 +1,16 @@
 // http://www.cs.up.ac.za/users/image/u16169532
+
+function changeImage(){
+	setInterval(cf,5000);
+}
+
+function randomA(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function cf() {
+	alert('hello');
+  img = document.getElementById('userImage');
+  user = 'http://www.cs.up.ac.za/users/image/';
+
+}
